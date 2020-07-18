3a0b83c669db3da8fa22dfdd19063e208da6d555
jonDorito <jonDorito@users.noreply.github.com>
2020-07-17 19:06:30 -0600

    Spaces between menus and more
    - Added spaces between each submenu in the program.
    - Made corrections to the spanish part of the program.

--- TransLog.pl
+++ TransLog.pl
@@ -8,6 +8,7 @@
 %         will be english or spanish.
 %_____________________________________________
 transLog:-
+    nl,
     write("Welcome to TransLog/Bienvenido a TransLog"),
     nl,
     write("-For english please enter the number 1 followed by a period (.)"),
@@ -20,6 +21,7 @@ transLog:-
     subMenu(Language).
 
 subMenu(Language):-
+    nl,
     sub_menu_language_loader(Language, [Main_Message, Option_1, Option_2, Option_3]),
     write(Main_Message),
     nl,
@@ -41,6 +43,7 @@ subMenu(Language):-
 %         translator module
 % _____________________________________________
 translate('english', Language):-
+    nl,
     translate_language_loader('english', Language, Main_Message),
     write(Main_Message),
     nl,
@@ -48,6 +51,7 @@ translate('english', Language):-
     nl,
     ask_translate_ESP(Language, Input).
 translate('spanish', Language):-
+    nl,
     translate_language_loader('spanish', Language, Main_Message),
     write(Main_Message),
     nl,
@@ -55,11 +59,13 @@ translate('spanish', Language):-
     nl,
     ask_translate_ENG(Language, Input).
 translate(3, Language):-
+    nl,
     translate_language_loader(3, Language, Main_Message),
     write(Main_Message),
     nl,
     transLog.
 translate(_, Language):-
+    nl,
     translate_language_loader(Language, Main_Message),
     write(Main_Message),
     nl,
@@ -76,6 +82,7 @@ translate(_, Language):-
 %_____________________________________________
 
 fail_translation(Language):-
+    nl,
     fail_translation_language_loader(Language, Message),
     write(Message),
     nl.
@@ -89,6 +96,7 @@ fail_translation(Language):-
 %_____________________________________________
 
 ask_translate_ESP(Language,'salir'):-
+    nl,
     closing_language_loader(Language, [Session_Message, Session_Back_To_Menu]),
     write(Session_Message),
     nl,
@@ -111,6 +119,7 @@ ask_translate_ESP(Language, _):-fail_translation(Language),
 %_____________________________________________
 
 ask_translate_ENG(Language,'salir'):-
+    nl,
     closing_language_loader(Language, [Session_Message, Session_Back_To_Menu]),
     write(Session_Message),
     nl,
@@ -133,6 +142,7 @@ ask_translate_ENG(Language, _):-fail_translation(Language),
 %_____________________________________________
 
 write_translate_ESP(Language, Output):-
+    nl,
     write(Output),
     nl,
     write_translation_language_loader(Language, Message),
@@ -150,6 +160,7 @@ write_translate_ESP(Language, Output):-
 %         NewInput - User's next instruction to the program
 %_____________________________________________
 write_translate_ENG(Language, Output):-
+    nl,
     write(Output),
     nl,
     write_translation_language_loader(Language, Message),
@@ -187,11 +198,11 @@ sub_menu_language_loader(Language, [Main_Message, Option_1, Option_2, Option_3])
 sub_menu_main_message('english', "Which language would you like to translate to?").
 sub_menu_main_message('spanish', "A que lenguaje desea traducir").
 
-sub_menu_option_1('english', "-Enter 1 for translating to spanish to english").
-sub_menu_option_1('spanish', "-Ingrese 1 para traducir de espanol a ingles").
+sub_menu_option_1('english', "-Enter 1 for translating from spanish to english").
+sub_menu_option_1('spanish', "-Ingrese 1 para traducir de español a ingles").
 
-sub_menu_option_2('english', "-Enter 2 for translating to english to spanish").
-sub_menu_option_2('spanish', "-Ingrese 2 para traducir de ingles a espanol").
+sub_menu_option_2('english', "-Enter 2 for translating from english to spanish").
+sub_menu_option_2('spanish', "-Ingrese 2 para traducir de ingles a español").
 
 sub_menu_option_3('english', "-Enter 3 for returning to the main menu").
 sub_menu_option_3('spanish', "-Ingrese 3 para returnar al menu principal").
@@ -214,10 +225,10 @@ translate_language_loader(Language, Main_Message):-
 % translateMenu Database
 %_____________________________________________
 translate_english_option('english', 'english', "Enter the word, sentence or phrase the you wanna to translate to english").
-translate_english_option('english', 'spanish', "Ingrese la palabra, oracion o frase que desea traducir al ingles").
+translate_english_option('english', 'spanish', "Ingrese la palabra, oración o párafo que desea traducir al ingles").
 
 translate_spanish_option('spanish', 'english', "Enter the word, sentence or phrase that you wanna to translate to spanish").
-translate_spanish_option('spanish', 'spanish', "Ingrese la palabra, oracion o frase que desea traducir al espanol").
+translate_spanish_option('spanish', 'spanish', "Ingrese la palabra, oración o párafo que desea traducir al español").
 
 translate_exit_option('english', "Returning to main menu...").
 translate_exit_option('spanish', "Regresando al menu principal...").
@@ -240,7 +251,7 @@ closing_language_loader(Language, [Session_Message, Session_Back_To_Menu]):-
 % closing_session Database
 %_____________________________________________
 closing_session_message('english', "This traslating sesion has been closed").
-closing_session_message('spanish', "Esta sesion de traduccion se ha cerrado").
+closing_session_message('spanish', "Esta sesión de traducción se ha cerrado").
 
 closing_session_back_to_menu('english', "Returning to main menu...").
 closing_session_back_to_menu('spanish', "Regresando al menu principal...").
@@ -265,4 +276,4 @@ write_translation_language_loader(Language, Message):-
     write_translation_message(Language, Message).
 
 write_translation_message('english', "Enter another word, sentence or paragraph to translate or enter the word 'salir' to exit to main menu").
-write_translation_message('spanish', "Ingrese otra palabra, oracion o parafo para traducir o bien, ingrese la palabra 'salir' para ir al menu principal").
+write_translation_message('spanish', "Ingrese otra palabra, oración o párafo para traducir o bien, ingrese la palabra 'salir' para ir al menu principal").
